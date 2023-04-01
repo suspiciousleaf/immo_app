cb3779aa81b2aded8ff27ac0dd07ac6418c8a97f
David <d.f.brooks01@gmail.com>
2023-04-01 21:52:22 +0200

    Rebuilt agent and type search functions, changed inc default to true

--- flask_app.py
+++ flask_app.py
@@ -62,33 +62,39 @@ def search_call():
         except:
             return 0
 
-    inc_none_beds_req = request.args.get('inc_none_beds') == "true"
+    inc_none_beds_req = not request.args.get('inc_none_beds') == "false"
 
     min_beds_req = try_min(request.args.get('min_beds'))
     max_beds_req = try_max(request.args.get('max_beds'))
+
+    inc_none_rooms_req = not request.args.get('inc_none_rooms') == "false"
+
+    min_rooms_req = try_min(request.args.get('min_rooms'))
+    max_rooms_req = try_max(request.args.get('max_rooms'))
+
     min_price_req = try_min(request.args.get('min_price'))
     max_price_req = try_max(request.args.get('max_price'))
 
     agent_list_req = try_csv(request.args.get('agents'))
     type_list_req = try_csv(request.args.get('types'))
 
-    inc_none_plot_req = request.args.get('inc_none_plot') == "true"
+    inc_none_plot_req = not request.args.get('inc_none_plot') == "false"
 
     min_plot_req = try_min(request.args.get('min_plot'))
     max_plot_req = try_max(request.args.get('max_plot'))
 
-    inc_none_size_req = request.args.get('inc_none_size') == "true"
+    inc_none_size_req = not request.args.get('inc_none_size') == "false"
 
     min_size_req = try_min(request.args.get('min_size'))
     max_size_req = try_max(request.args.get('max_size'))
 
     search_radius_req = try_search_radius(request.args.get('search_radius'))
-    inc_none_location_req = request.args.get('inc_none_location') == "true"
+    inc_none_location_req = not request.args.get('inc_none_location') == "false"
     towns_req = try_csv(request.args.get('town'))
 
-    return search(listings = listings, type_list = type_list_req, agent_list = agent_list_req, towns = towns_req, inc_none_location = inc_none_location_req, search_radius = search_radius_req, inc_none_beds = inc_none_beds_req, min_beds = min_beds_req, max_beds = max_beds_req, min_price = min_price_req, max_price = max_price_req, inc_none_plot = inc_none_plot_req, min_plot = min_plot_req, max_plot = max_plot_req, inc_none_size = inc_none_size_req, min_size = min_size_req, max_size = max_size_req)
+    return search(listings = listings, type_list = type_list_req, agent_list = agent_list_req, towns = towns_req, inc_none_location = inc_none_location_req, search_radius = search_radius_req, inc_none_beds = inc_none_beds_req, min_beds = min_beds_req, max_beds = max_beds_req, inc_none_rooms = inc_none_rooms_req, min_rooms = min_rooms_req, max_rooms = max_rooms_req, min_price = min_price_req, max_price = max_price_req, inc_none_plot = inc_none_plot_req, min_plot = min_plot_req, max_plot = max_plot_req, inc_none_size = inc_none_size_req, min_size = min_size_req, max_size = max_size_req)
 
-# search(listings, type_list = type_list, agent_list = agent_list, town = "", search_radius = 0, inc_none_beds = True, min_beds = 0, max_beds = math.inf, min_price = 0, max_price = math.inf,  inc_none_plot = True, min_plot = 0, max_plot = math.inf, inc_none_size = True, min_size = 0, max_size = math.inf)
+# Search parameters: Type, Agent, location, search radius, bedroom number, room number, price, plot size, property size
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
--- json_search.py
+++ json_search.py
@@ -28,58 +28,85 @@ agent_dict = {
 }
 
 def filter_price(results, min_price, max_price):
+    print("filter_price ran")
     return [x for x in results if min_price <= x["price"] <= max_price]
 
-def filter_agent_type(results, type_list, agent_list):
-    fullname_agent_search = [agent_dict[agent] for agent in agent_list]
-    return [x for x in results if x["types"].casefold() in type_list and x["agent"] in fullname_agent_search]
+def filter_agent(results, agent_list):
+    print("filter_agent ran")
+    if agent_list:
+        fullname_agent_search = [agent_dict[agent] for agent in agent_list]
+        return [x for x in results if x["agent"] in fullname_agent_search]
+    else:
+        return results
+
+def filter_type(results, type_list):
+    print("filter_type ran")
+    if type_list:
+        return [x for x in results if x["types"].casefold() in type_list]
+    else:
+        return results
 
 def filter_beds(results, inc_none_beds, min_beds, max_beds):
+    print("filter_beds ran")
     if inc_none_beds == True:
         return [x for x in results if x["bedrooms"] == None or min_beds <= x["bedrooms"] <= max_beds]
     elif inc_none_beds == False:
         return [x for x in results if x["bedrooms"] != None and min_beds <= x["bedrooms"] <= max_beds]
+    
+def filter_rooms(results, inc_none_rooms, min_rooms, max_rooms):
+    print("filter_rooms ran")
+    if inc_none_rooms == True:
+        return [x for x in results if x["bedrooms"] == None or min_rooms <= x["bedrooms"] <= max_rooms]
+    elif inc_none_rooms == False:
+        return [x for x in results if x["bedrooms"] != None and min_rooms <= x["bedrooms"] <= max_rooms]    
 
 def filter_plot(results, inc_none_plot, min_plot, max_plot):
+    print("filter_plot ran")
     if inc_none_plot == True:
         return [x for x in results if x["plot"] == None or min_plot <= x["plot"] <= max_plot]
     elif inc_none_plot == False:
         return [x for x in results if x["plot"] != None and min_plot <= x["plot"] <= max_plot]
 
 def filter_size(results, inc_none_size, min_size, max_size):
+    print("filter_size ran")
     if inc_none_size == True:
         return [x for x in results if x["size"] == None or min_size <= x["size"] <= max_size]
     elif inc_none_size == False:
         return [x for x in results if x["size"] != None and min_size <= x["size"] <= max_size]
 
 def get_distance(origin, destination):
+    print("get_distance ran")
     origin = gps_dict[origin]
     destination = gps_dict[destination]
     return distance(origin, destination).km
 
 def filter_location(results, towns, search_radius, inc_none_location):
-    location_results = []
-    for town in towns:
-        for result in results:
-            if inc_none_location == True:
-                if result["town"] == None:
-                    location_results.append(result)
-                else:
-                    if unidecode(result["town"].casefold().replace("-", " ").replace("l hers", "l'hers").replace("d olmes", "d'olmes").replace("val du faby", "esperaza").replace("l'aiguillon", "l' aiguillon").replace("l'isle en dodon", "l' isle en dodon")) not in town_list_clean:
+    print("filter_location ran")
+    if towns:
+        location_results = []
+        for town in towns:
+            for result in results:
+                if inc_none_location == True:
+                    if result["town"] == None:
                         location_results.append(result)
                     else:
-                        if get_distance(town.casefold(), unidecode(result["town"].casefold().replace("-", " ").replace("l hers", "l'hers").replace("d olmes", "d'olmes").replace("val du faby", "esperaza").replace("l'aiguillon", "l' aiguillon").replace("l'isle en dodon", "l' isle en dodon"))) <= search_radius:
+                        if unidecode(result["town"].casefold().replace("-", " ").replace("l hers", "l'hers").replace("d olmes", "d'olmes").replace("val du faby", "esperaza").replace("l'aiguillon", "l' aiguillon").replace("l'isle en dodon", "l' isle en dodon")) not in town_list_clean:
                             location_results.append(result)
-            elif inc_none_location == False:
-                if result["town"] == None:
-                    pass
-                else:
-                    if unidecode(result["town"].casefold().replace("-", " ").replace("l hers", "l'hers").replace("d olmes", "d'olmes").replace("val du faby", "esperaza").replace("l'aiguillon", "l' aiguillon").replace("l'isle en dodon", "l' isle en dodon")) not in town_list_clean:
+                        else:
+                            if get_distance(town.casefold(), unidecode(result["town"].casefold().replace("-", " ").replace("l hers", "l'hers").replace("d olmes", "d'olmes").replace("val du faby", "esperaza").replace("l'aiguillon", "l' aiguillon").replace("l'isle en dodon", "l' isle en dodon"))) <= search_radius:
+                                location_results.append(result)
+                elif inc_none_location == False:
+                    if result["town"] == None:
                         pass
                     else:
-                        if get_distance(town.casefold(), unidecode(result["town"].casefold().replace("-", " ").replace("l hers", "l'hers").replace("d olmes", "d'olmes").replace("val du faby", "esperaza").replace("l'aiguillon", "l' aiguillon").replace("l'isle en dodon", "l' isle en dodon"))) <= search_radius:
-                            location_results.append(result)
-    return location_results 
+                        if unidecode(result["town"].casefold().replace("-", " ").replace("l hers", "l'hers").replace("d olmes", "d'olmes").replace("val du faby", "esperaza").replace("l'aiguillon", "l' aiguillon").replace("l'isle en dodon", "l' isle en dodon")) not in town_list_clean:
+                            pass
+                        else:
+                            if get_distance(town.casefold(), unidecode(result["town"].casefold().replace("-", " ").replace("l hers", "l'hers").replace("d olmes", "d'olmes").replace("val du faby", "esperaza").replace("l'aiguillon", "l' aiguillon").replace("l'isle en dodon", "l' isle en dodon"))) <= search_radius:
+                                location_results.append(result)
+        return location_results 
+    else:
+        return results
 
 all_agents_set = set()
 all_types_set = set()
@@ -90,25 +117,39 @@ for item in listings:
 
 all_agent_list = list(all_agents_set)
 all_type_list = list(all_types_set)
-print(all_type_list)
+# print(all_type_list)
 
-def search(listings, type_list = all_type_list, agent_list = ['ami', 'mm', 'richardson', "l'immo", 'arthur', 'jammes', 'nestenn', 'cimm', 'api', 'aude', 'time'], inc_none_location = False, towns = None, search_radius = 0, inc_none_beds = True, min_beds = 0, max_beds = math.inf, min_price = 0, max_price = math.inf,  inc_none_plot = True, min_plot = 0, max_plot = math.inf, inc_none_size = True, min_size = 0, max_size = math.inf):
+# types = ['terrain', 'immeuble', 'appartement', 'commerce', 'maison']
+# agent_list = ['ami', 'mm', 'richardson', "l'immo", 'arthur', 'jammes', 'nestenn', 'cimm', 'api', 'aude', 'time']
+
+
+def search(listings, type_list = None, agent_list = None, inc_none_location = False, towns = None, search_radius = 0, inc_none_beds = True, min_beds = 0, max_beds = math.inf, inc_none_rooms = True, min_rooms = 0, max_rooms = math.inf, min_price = 0, max_price = math.inf,  inc_none_plot = True, min_plot = 0, max_plot = math.inf, inc_none_size = True, min_size = 0, max_size = math.inf):
+    print(len(listings))
     results_list = filter_price(listings, min_price, max_price)
-    results_list = filter_agent_type(results_list, type_list, agent_list)
+    print(len(results_list))
+    results_list = filter_agent(results_list, agent_list)
+    print(len(results_list))
+    results_list = filter_type(results_list, type_list)
+    print(len(results_list))
     results_list = filter_beds(results_list, inc_none_beds, min_beds, max_beds)
+    print(len(results_list))
+    results_list = filter_rooms(results_list, inc_none_rooms, min_rooms, max_rooms)
+    print(len(results_list))
     results_list = filter_plot(results_list, inc_none_plot, min_plot, max_plot)
+    print(len(results_list))
     results_list = filter_size(results_list, inc_none_size, min_size, max_size)
-    if towns:
-        results_list = filter_location(results_list, towns, search_radius, inc_none_location)
+    print(len(results_list))
+    results_list = filter_location(results_list, towns, search_radius, inc_none_location)
+    print(len(results_list))
 
     return results_list
 
 
 # pprint(agent_list)
 
-# pprint(type_list)
+# pprint(all_type_list)
 
-# results_list = (search(listings, type_list=["Terrain"]))
+# results_list = (search(listings, type_list=["immeuble"]))
 # pprint(results_list)
 
 
