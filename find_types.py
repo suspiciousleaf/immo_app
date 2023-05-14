2f28eaaeb6a511a895288dca7ef1a6c7d105343d
David <d.f.brooks01@gmail.com>
2023-05-14 14:13:24 +0200

    Fixed bug with Cimm terrain area

--- find_types.py
+++ find_types.py
@@ -11,4 +11,16 @@ types = set()
 for listing in listings:
     types.add(listing["types"])
 
-pprint(types)
+print("\n", types, sep="")
+
+
+# Code below checks for duplicate listings in listings.json
+urls = []
+for listing in listings:
+    urls.append(listing["link_url"])
+url_set = set(urls)
+
+if len(urls) == len(url_set):
+    print("\nNo duplicates found.")
+elif len(urls) > len(url_set):
+    print("Duplicate listings found.")
